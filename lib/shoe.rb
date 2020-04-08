889a341de5665470863af260e603941f98bc6d3b
chic-caller-7520 <arbrown110@gmail.com>
2020-04-08 18:03:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
-  attr_accessor :color, :size, :material, :conditon,
+  attr_accessor :color, :size, :material, :conditon
   attr_reader :brand
   def initialize(brand)
     @brand = brand
   end
   def cobble
-    puts "Flipping the page...wow, you read fast!"
+    puts "Your shoe is as good as new!"
   end  
 
 end
