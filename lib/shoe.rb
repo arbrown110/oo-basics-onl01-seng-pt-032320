16b6323239adcd3b50ee1442a4e6ad8244cdddad
chic-caller-7520 <arbrown110@gmail.com>
2020-04-08 17:55:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,14 @@
+class Shoe
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  def initialize(title)
+    @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end  
 
-
-
-
-
-
-
+end
 
 
 
